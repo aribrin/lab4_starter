8b3cb6c4e1c93d54b4955fe5ced3f081fdb701e3
bwchin <b5chin@ucsd.edu>
2023-03-01 16:16:10 -0800

    Update seqsm.sv

--- seqsm.sv
+++ seqsm.sv
@@ -17,7 +17,7 @@ module seqsm
    // TODO:  1: Idle -> 
    // TODO:  2: LoadPreamble (read the preamble from the ROM and capture it in some registers) ->
    // TODO:  3: LoadTaps (read the taps from the ROM and capture it in some registers) ->
-   // TODO:  4: LoadSeed (read the taps from the ROM and capture it in some registers) ->
+   // TODO:  4: LoadSeed (read the seed from the ROM and capture it in some registers) ->
    // TODO:  5: InitLFSR (load the LFSR with the taps and seed) ->
    // TODO:  6: ProcessPremble (encrypt preamble until byteCount is the same as preamble length) ->
    // TODO:  7: Encrypt rest of packet until byteCount == 32 (max packet length)
